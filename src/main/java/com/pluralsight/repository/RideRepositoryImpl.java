6b5bf1686e279e4876d008d8759905ea559fa6a1
Dimitar Garbelyanski <dimitar.garbelyanski@spidex.co.uk>
2020-04-06 14:47:21 +0100

    simplejdbcinsert

--- src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
+++ src/main/java/com/pluralsight/repository/RideRepositoryImpl.java
@@ -1,14 +1,22 @@
 package com.pluralsight.repository;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.simple.SimpleJdbcInsert;
 import org.springframework.stereotype.Repository;
 
 import com.pluralsight.model.Ride;
 
 @Repository("rideRepository")
 public class RideRepositoryImpl implements RideRepository {
+
+	@Autowired
+	private JdbcTemplate jdbcTemplate;
 
 	@Override
 	public List<Ride> getRides() {
@@ -19,5 +27,29 @@ public class RideRepositoryImpl implements RideRepository {
 		rides.add(ride);
 		return rides;
 	}
-	
+
+	@Override
+	public Ride createRide(Ride ride) {
+		//jdbcTemplate.update("insert into ride(name, duration) values (?, ?)",ride.getName(), ride.getDuration());
+
+		SimpleJdbcInsert insert = new SimpleJdbcInsert(jdbcTemplate);
+		List<String> columns = new ArrayList<>();
+		columns.add("name");
+		columns.add("duration");
+		insert.setTableName("ride");
+		insert.setColumnNames(columns);
+
+		Map<String, Object> data = new HashMap<>();
+		data.put("name", ride.getName());
+		data.put("duration", ride.getDuration());
+
+		insert.setGeneratedKeyName("id");
+
+		Number key = insert.executeAndReturnKey(data);
+
+		System.out.println("Key:"+ key);
+
+		return null;
+	}
+
 }
--- src/test/java/com/pluralsight/controller/RestControllerTest.java
+++ src/test/java/com/pluralsight/controller/RestControllerTest.java
@@ -13,7 +13,18 @@ import org.junit.Test;
 
 public class RestControllerTest {
 
-	@Test(timeout=3000)
+	@Test(timeout=10000)
+	public void testCreateRide() {
+		RestTemplate restTemplate = new RestTemplate();
+
+		Ride ride = new Ride();
+		ride.setName("Round valley ride");
+		ride.setDuration(22);
+
+		restTemplate.put("http://localhost:8080/ride_tracker/ride", ride);
+	}
+
+	@Test(timeout=10000)
 	public void testGetRides() {
 		RestTemplate restTemplate = new RestTemplate();
 
